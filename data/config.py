e417d6bba6e338259e7303e43418d48ef70ea362
roots <akramovabdullo2002@gmail.com>
2023-11-28 16:35:44 +0500

    delete pipfile and install venv requirements.txt

--- data/config.py
+++ data/config.py
@@ -1,10 +1,7 @@
-from environs import Env
+import os
+from dotenv import load_dotenv
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+load_dotenv()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+ADMINS = os.getenv('ADMINS')
